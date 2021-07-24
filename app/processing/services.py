958e28b65898d33037fb289fe35fa3157b7fe365
Alexander Vlasov <18brakanier.forest@gmail.com>
2021-07-24 17:54:44 +0300

    remove audio temp files, add context

--- app/processing/services.py
+++ app/processing/services.py
@@ -38,59 +38,56 @@ def create_preview(video_id: int) -> None:
 def encode_to_mp4(video_id: int) -> None:
     video_model = models.Video.objects.get(id=video_id)
 
-    video = editor.VideoFileClip(video_model.raw_video.path)
-    new_video = resize.resize(video, (640, 360))
+    with editor.VideoFileClip(video_model.raw_video.path) as video:
+        new_video = resize.resize(video, (640, 360))
 
-    basename, _ = os.path.splitext(os.path.basename(video_model.raw_video.path))
+        basename, _ = os.path.splitext(os.path.basename(video_model.raw_video.path))
 
-    location = video_model.video_mp4.storage.location
-    upload_to = models.Video.video_mp4.field.upload_to
+        location = video_model.video_mp4.storage.location
+        upload_to = models.Video.video_mp4.field.upload_to
 
-    filename = f'{basename}.mp4'
+        filename = f'{basename}.mp4'
 
-    if upload_to:
-        file_name_for_save = f'{upload_to}/{filename}'
-        path = os.path.join(location, upload_to, filename)
-        dir_path = os.path.join(location, upload_to)
-    else:
-        file_name_for_save = filename
-        path = os.path.join(location, filename)
-        dir_path = location
+        if upload_to:
+            file_name_for_save = f'{upload_to}/{filename}'
+            path = os.path.join(location, upload_to, filename)
+            dir_path = os.path.join(location, upload_to)
+        else:
+            file_name_for_save = filename
+            path = os.path.join(location, filename)
+            dir_path = os.path.join(location)
 
-    temp_audiofile = os.path.join(location, f'{basename}.mp3')
-
-    create_dirs(dir_path)
-    new_video.write_videofile(path, temp_audiofile=temp_audiofile)
-    video_model.set_video_mp4_path(file_name_for_save)
+        create_dirs(dir_path)
+        new_video.write_videofile(path)
+        video_model.set_video_mp4_path(file_name_for_save)
 
 
 def encode_to_webm(video_id: int) -> None:
     video_model = models.Video.objects.get(id=video_id)
 
-    video = editor.VideoFileClip(video_model.raw_video.path)
-    new_video = resize.resize(video, (640, 360))
+    with editor.VideoFileClip(video_model.raw_video.path) as video:
+        new_video = resize.resize(video, (640, 360))
 
-    basename, _ = os.path.splitext(os.path.basename(video_model.raw_video.path))
+        basename, _ = os.path.splitext(os.path.basename(video_model.raw_video.path))
 
-    location = video_model.video_webm.storage.location
-    upload_to = models.Video.video_webm.field.upload_to
+        location = video_model.video_webm.storage.location
+        upload_to = models.Video.video_webm.field.upload_to
 
-    filename = f'{basename}.webm'
+        filename = f'{basename}.webm'
 
-    if upload_to:
-        file_name_for_save = f'{upload_to}/{filename}'
-        path = os.path.join(location, upload_to, filename)
-        dir_path = os.path.join(location, upload_to)
-    else:
-        file_name_for_save = filename
-        path = os.path.join(location, filename)
-        dir_path = location
+        if upload_to:
+            file_name_for_save = f'{upload_to}/{filename}'
+            path = os.path.join(location, upload_to, filename)
+            dir_path = os.path.join(location, upload_to)
+        else:
+            file_name_for_save = filename
+            path = os.path.join(location, filename)
+            dir_path = os.path.join(location)
 
-    temp_audiofile = os.path.join(location, f'{basename}.mp3')
 
-    create_dirs(dir_path)
-    new_video.write_videofile(path, temp_audiofile=temp_audiofile)
-    video_model.set_video_webm_path(file_name_for_save)
+        create_dirs(dir_path)
+        new_video.write_videofile(path)
+        video_model.set_video_webm_path(file_name_for_save)
 
 
 def create_dirs(path: str):
